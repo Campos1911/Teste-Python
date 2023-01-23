4ecd5d51d16ee3f0640e01b5b93c1db30f16d44b
Campos1911 <anthonithomes@gmail.com>
2023-01-23 10:36:41 -0300

    Add

---
+++ Teste.py
@@ -0,0 +1,51 @@
+print("\n\n\tHello, World\n")
+
+#Isso aqui é um comentário
+
+#Variáveis
+a = 19
+b = "Anthoni"
+
+#Printando variáveis
+print(a)
+print(b)
+
+#Descobrindo o tipo das variáveis
+print(type(a))
+print(type(b))
+print("\n")
+
+#Muitos valores em uma variável
+
+carros = ["volvo", "mercedes", "chevrolet"]
+x, y, z = carros
+
+print(x)
+print(y)
+print(z)
+print("\n")
+
+#Imprimir variáveis inteiras
+
+t = "Anthoni tem 19 anos"
+print(t)
+print("\n")
+
+#Funções
+
+o = "Isso é um teste de funções"
+
+def func():
+    print("Testando a variável: " + o)
+func()
+print("\n")
+
+#Tipos de números
+
+r = 1 #Int
+rr = 2.2 #Float
+rrr = 3j #Complexo
+
+print(type(r))
+print(type(rr))
+print(type(rrr))
